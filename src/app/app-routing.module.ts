d0ae6179aaa8a92381a66e54af78b258cbde17bf
matheoszxx <matheuszxx1@outlook.com>
2023-10-04 23:43:21 -0300

    criando as primeiras rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //1º - passamos o "path" e deixamos a rota vazia para acessarmos a tela principal "index"
+  //2º - passamos o componente em que vai ta acessando
+  //3º - passamos o "pathMatch" e colocamos dentro dele full, pois no path passamos a rota vazia
+  {path: '', component: TitleComponent, pathMatch: 'full'},
+  {path: 'portfolio', component: CardComponent, pathMatch: 'prefix'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
