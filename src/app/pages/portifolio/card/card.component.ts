95708594d9dcb9d5c7baff29720d2b216aff373c
matheus <matheuszxx1@outlook.com>
2023-10-05 09:21:23 -0300

    Redirecionamento por componente usando 'SetInterval'

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -10,7 +10,10 @@ export class CardComponent implements OnInit {
 
   //fazendo injeção de dependencias
   //http://localhost:4200/portfolio/{result == 1}
-  constructor(private parametrizador: ActivatedRoute) {
+  constructor(
+    private parametrizador: ActivatedRoute,
+    private navegador: Router
+    ) {
     //aqui estamos usando o "subscribe" para observar mudanças em um objeto observavel que é o "params"
     this.parametrizador.params.subscribe(result => console.log(result))
 
@@ -18,6 +21,10 @@ export class CardComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    //após 5000ms vai ser redirecionado (setInterval serve para dar um intervalo)
+    setInterval(()=> {
+      this.navegador.navigate(['/'])
+    }, 5000)
   }
 
 }
