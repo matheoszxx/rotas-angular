12bfe2de5147ecccc7f20d049276cf27101ecc08
matheus <matheuszxx1@outlook.com>
2023-10-05 09:14:49 -0300

    Recuperando QueryParams de rotas

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -9,9 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 export class CardComponent implements OnInit {
 
   //fazendo injeção de dependencias
+  //http://localhost:4200/portfolio/{result == 1}
   constructor(private parametrizador: ActivatedRoute) {
+    //aqui estamos usando o "subscribe" para observar mudanças em um objeto observavel que é o "params"
     this.parametrizador.params.subscribe(result => console.log(result))
 
+    this.parametrizador.queryParams.subscribe(result => console.log(result))
   }
 
   ngOnInit(): void {
