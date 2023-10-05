a2c99ca42eb03fd65ba7e5a211c56c6a4d78fa25
matheus <matheuszxx1@outlook.com>
2023-10-05 11:00:40 -0300

    Parametros de rotas children

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -9,14 +9,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CardComponent implements OnInit {
 
   //fazendo injeção de dependencias
-  //http://localhost:4200/portfolio/{result == 1}
   constructor(
     private parametrizador: ActivatedRoute,
     private navegador: Router
     ) {
     //aqui estamos usando o "subscribe" para observar mudanças em um objeto observavel que é o "params"
+    //http://localhost:4200/portfolio/{result == 1}
     this.parametrizador.params.subscribe(result => console.log(result))
 
+    this.parametrizador.firstChild?.params.subscribe(result => console.log(result))
+
+    //http://localhost:4200/portfolio/1?name=matheus&token=123
     this.parametrizador.queryParams.subscribe(result => console.log(result))
   }
 
